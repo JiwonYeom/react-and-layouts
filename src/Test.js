711de4a6aa553d769ac4db82669b868c8dbe9ab8
JiwonYeom <jwyeom63@gmail.com>
2017-12-09 15:58:14 +0900

    set up webpack environment for react

--- src/Test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Test = (props) => {
-  return (<div className='test'>This is a test component no.{props.number}</div>);
-};
-
-export default Test;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist"); // transpiled files stored
+var SRC_DIR = path.resolve(__dirname, "src"); // untranspiled files
+
+var config = {
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/" // what would be the public folder
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
