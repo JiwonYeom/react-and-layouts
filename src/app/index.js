bcf4f2e65019470b35d8740e2215c7573d544df1
JiwonYeom <jwyeom63@gmail.com>
2017-12-09 17:53:11 +0900

    first component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,21 @@
-console.log("it works! yey!");
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+// got a component
+    render() {      
+        return (        // JSX begins. Remember one component, one root element
+            <div className="container">
+                <div className="row">      
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <h1>Hello!</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+// className instead of class
+// no comments within JSX!
+
+render(<App/>, window.document.getElementById("app"));
