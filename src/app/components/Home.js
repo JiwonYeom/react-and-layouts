668abfe7c21f602e87ce6bd1195100519943bc1f
JiwonYeom <jwyeom63@gmail.com>
2017-12-10 21:42:15 +0900

    react props handling & type check

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,11 +1,40 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export class Home extends React.Component {
     render() {
+        // can add additional parts for dynamic data in {}
+        // let test = "";
+        // if (true) {
+        //     test = <p>Hello!</p>;
+        // }
+
+        // to use props
+        // this.props
+
+        // when looping, don't forget to let react track it with key    
+
+        let text = "here goes!";
         return (
             <div>
-                <p>new component</p>
+                <p>Hi {this.props.name}!</p>
+                <div>
+                    <ul>
+                        {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
+                    </ul>
+                </div>
+                <hr />
+                {this.props.children}
             </div>
         );
     }
 }
+
+
+// setting certain types for each props (type checking)
+Home.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    user: PropTypes.object,
+    children: PropTypes.element.isRequired
+};
--- src/app/index.js
+++ src/app/index.js
@@ -7,6 +7,10 @@ import { Home } from "./components/Home";
 class App extends React.Component {
 // got a component
     render() {      
+        let user = {
+            name: "Jamie",
+            hobbies: ["Reading","Running","Play Music"]
+        };
         return (        // JSX begins. Remember one component, one root element
             <div className="container">
                 <div className="row">      
@@ -16,7 +20,9 @@ class App extends React.Component {
                 </div>
                 <div className="row">      
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home/>
+                        <Home name={"JW"} age={27} user={user}>
+                            <p>test children prop</p>
+                        </Home>
                     </div>
                 </div>
             </div>
