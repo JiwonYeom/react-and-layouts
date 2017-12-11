2d03ee43a088a6e2b56cb029490377e276943d7e
JiwonYeom <jwyeom63@gmail.com>
2017-12-11 22:21:58 +0900

    state and how react renders

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -4,21 +4,29 @@ import PropTypes from "prop-types";
 export class Home extends React.Component {
     constructor(props) {
         super();
-        this.age = props.age;
+        this.state = {
+            age: props.initialAge,
+            status: 0
+        };
     }
 
     //declare function
     onMakeOlder(){
+        this.setState({
+            age: this.state.age + 3
+        }); // this will trigger re-rendering of UI
+
         // without .bind, this here will refer to the button instead of class
         // this.onMakeOlder.bind(this) == ()=> this.onMakeOlder()
-        this.age +=3;
-        console.log(this.age); // you will see that it is getting incremented. But the state has not changed.
+        // this.age +=3;
+        // console.log(this.age); // you will see that it is getting incremented. But the state has not changed.
     }
 
     render() {
         return (
             <div>
-                <p>Hi {this.props.name}!, your age is {this.age}</p>
+                <p>Hi {this.props.name}!, your age is {this.state.age}</p>
+                <p>Status: {this.state.status}</p>
                 <hr/>
                 <button onClick={()=> this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
             </div>
@@ -30,5 +38,5 @@ export class Home extends React.Component {
 // setting certain types for each props (type checking)
 Home.propTypes = {
     name: PropTypes.string,
-    age: PropTypes.number,
+    initialAge: PropTypes.number,
 };
