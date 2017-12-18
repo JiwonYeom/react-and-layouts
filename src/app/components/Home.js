b316beda59abda1b820fcc7ba2bc5cf3e52a0be0
JiwonYeom <jwyeom63@gmail.com>
2017-12-18 20:04:59 +0900

    route basic exercise

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,70 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export class Home extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-            age: props.initialAge,
-            status: 0,
-            // set default value from props value
-            homeLink: props.initialLinkName
-        };
-    }
-
-    //declare function
-    onMakeOlder() {
-        this.setState({
-            age: this.state.age + 3
-        }); // this will trigger re-rendering of UI
-
-        // without .bind, this here will refer to the button instead of class
-        // this.onMakeOlder.bind(this) == ()=> this.onMakeOlder()
-        // this.age +=3;
-        // console.log(this.age); // you will see that it is getting incremented. But the state has not changed.
-
-        /* 
-        ReactJS uses the virtual DOM.
-        DOM written in JavaScript to maximize performance. It updates only the changed state.
-        ReactJS recreates virtual DOM, compare it with the OLD virtual DOM.
-        It will re-render the changed part.
-        */ 
-    }
-
-    onChangeLink() {
-        this.props.changeLink(this.state.homeLink);
-    }
-
-    //two-way binding
-    onChangeInput(event) {
-        this.setState({
-            homeLink: event.target.value
-        })
-
-    }
-
     render() {
         return (
             <div>
-                <p>Hi {this.props.name}!, your age is {this.state.age}</p>
-                <p>Status: {this.state.status}</p>
-                <hr/>
-                <button onClick={()=> this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
-                <hr/>
-                <button onClick={this.props.greet}>Greet</button>
-                <hr/>
-                <input type="text" value={this.state.homeLink} 
-                    onChange={(event) => this.onChangeInput(event)} />
-                <button onClick={this.onChangeLink.bind(this)}>Change Header Link</button>
+                <h3>Home</h3>
             </div>
         );
     }
 }
 
-
-// setting certain types for each props (type checking)
-Home.propTypes = {
-    name: PropTypes.string,
-    initialAge: PropTypes.number,
-    greet: PropTypes.func,
-    initialLinkName: PropTypes.string,
-};
--- src/app/index.js
+++ src/app/index.js
@@ -1,48 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
+import {Router, Route} from "react-router"
+import createBrowserHistory from 'history/createBrowserHistory'
 
-import { Header } from "./components/Header";
+import { Root } from "./components/Root";
 import { Home } from "./components/Home";
+import { User } from "./components/User";
 
+const customHistory = createBrowserHistory();
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            homeLink : "Home"
-        };
-    }
-
-    onGreet() {
-        alert("Hello!");
-    }
-
-    onChangeLinkName(newName) {
-        this.setState({
-            homeLink: newName
-        });
-    }
-
-    render() {      
-        return (        // JSX begins. Remember one component, one root element
-            <div className="container">
-                <div className="row">      
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Header homeLink={this.state.homeLink}/>
-                    </div>
+    render() {
+        return (
+            <Router history={customHistory}>
+                <div>
+                    <Route exact path="/" component={Root}/>
+                    <Route path="/user" component={User} />
+                    <Route path="/home" component={Home} />
                 </div>
-                <div className="row">      
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Home 
-                        name={"JW"} 
-                        initialAge={27} 
-                        greet={this.onGreet}
-                        changeLink ={this.onChangeLinkName.bind(this)}
-                        initialLinkName = {this.state.homeLink}
-                         />
-                    </div>
-                </div>
-            </div>
-        );
+            </Router>
+        )
     }
 }
 // className instead of class
